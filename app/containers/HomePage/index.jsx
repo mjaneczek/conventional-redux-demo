2e5c1e19b9a7dde2910b37fb32846823f3052221
Michal Janeczek <michal.janeczek@ymail.com>
2018-01-28 01:39:49 +0100

    Add first example component

--- app/containers/HomePage/index.jsx
+++ app/containers/HomePage/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Counter from './Counter';
 import CounterInteractor from './CounterInteractor';
+import Highlight from 'react-highlight';
 import { Helmet } from 'react-helmet';
 import { replaceDynamicInteractors } from "conventional-redux";
+import 'highlight.js/styles/agate.css';
 
 export default class HomePage extends React.Component {
   componentWillMount() {
@@ -19,8 +21,69 @@ export default class HomePage extends React.Component {
         </Helmet>
 
         <h1>Counter example - class component</h1>
-        <Counter />
+
+        <div className="card bg-light mb-4">
+          <div className="card-header">Component</div>
+          <div className="card-body">
+
+            <Counter />
+
+          </div>
+        </div>
+
+        <div className="card bg-dark text-white">
+          <div className="card-header">Code</div>
+          <div className="card-body">
+
+            <Highlight className='javascript' dangerousInnerHTML={true}>
+              {`
+// component
+class Counter extends React.Component {
+  render () {
+    return (
+      <div>
+        <h3>Current value: {this.p('counter')}</h3>
+
+        <button className="btn btn-primary" onClick={() => this.d('counter:increment')}>
+          Increment
+        </button>
+
+        <button className="btn btn-danger" onClick={() => this.d('counter:doubleAsync')}>
+          Double (Async)
+        </button>
       </div>
+    )
+  }
+}
+
+export default connectInteractors(Counter, ['counter']);
+
+// interactor
+export default class CounterInteractor {
+  defaultState() {
+    return 0;
+  }
+
+  doubleAsync() {
+    setTimeout(() => { this.dispatch('counter:double') }, 500)
+  }
+
+  onIncrement() {
+    return this.state + 1;
+  }
+
+  onDouble() {
+    return this.state * 2;
+  }
+}
+
+          `}
+        </Highlight>
+
+      </div>
+    </div>
+
+  </div>
     );
   }
 }
